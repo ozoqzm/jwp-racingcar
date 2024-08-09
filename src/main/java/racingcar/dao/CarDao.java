3387bf804dd417190dd9143ec6e3548dcf935b79
sojeong <spah7839@naver.com>
2024-08-09 21:37:59 +0900

    chore: 패키지 및 클래스 생성

---
+++ src/main/java/racingcar/dao/CarDao.java
@@ -0,0 +1,11 @@
+package racingcar.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CarDao {
+}
